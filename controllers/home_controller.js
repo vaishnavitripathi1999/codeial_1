ad53dcb24e4719bd0ee44ab18ec2203d924ad244
vaishnavitripathi1999 <simanttripathi@gmail.com>
2020-06-25 12:00:37 +0530

    updated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,14 @@
 
 module.exports.home=function(req,res){
 
-    return res.end('<h1>HEYYYYYYYY</h1>');
+    //this is to send the response directly 
+    // return res.end('<h1>HEYYYYYYYY</h1>');
+
+
+    //to render ejs file 
+
+    return res.render('home',{
+        title: "home  "
+    });
 
 }
