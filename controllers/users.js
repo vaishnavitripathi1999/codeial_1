e8269cf25a757c3c35164354fe01d5e61a1c705b
vaishnavitripathi1999 <simanttripathi@gmail.com>
2020-06-25 12:05:02 +0530

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,10 @@
 
 module.exports.profile=function(req,res){
 
-    return res.end('<h1>HERE IS YOUR PROFILE</h1>');
+    // return res.end('<h1>HERE IS YOUR PROFILE</h1>');
+
+    return res.render('user',{
+        title: 'user '
+    });
     
     }
