ba0afd77909f22dbcfe0b6d2645e755c01aa68b5
vaishnavitripathi1999 <simanttripathi@gmail.com>
2020-08-07 14:54:56 +0530

    converting the controllers into async

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,42 +1,76 @@
 const Post = require('../models/posts');
 const User = require('../models/user')
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
+// module.exports.home = function(req, res){
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 25);
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
+//     // Post.find({}, function(err, posts){
+//     //     return res.render('home', {
+//     //         title: "Codeial | Home",
+//     //         posts:  posts
+//     //     });
+//     // });
 
-    // populate the user of each post
-    Post.find({})
+//     // populate the user of each post
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }
+//     })
+//     .exec(function(err, posts){
+
+//         User.find({},function(err,user){
+
+//             return res.render('home', {
+//                 title: "Codeial | Home",
+//                 posts:  posts,
+//                 all_user:user
+//             });
+
+
+//         });
+
+        
+//     })
+
+// };
+
+// module.exports.actionName = function(req, res){}
+module.exports.home=async function(req,res){
+try{
+
+    let posts =await  Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
         populate: {
             path: 'user'
         }
-    })
-    .exec(function(err, posts){
+    });
 
-        User.find({},function(err,user){
+    let user=await User.find({},function(err,user){
 
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_user:user
-            });
-
-
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_user:user
         });
 
-        
-    })
+
+    });
+
+}catch(err){
+
+console.log('ERROR',err);
+return ;
+
+
 
 }
 
-// module.exports.actionName = function(req, res){}
+
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,33 +1,33 @@
 const Post = require('../models/posts');
 const Comments=require('../models/comments');
 
-module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('error in creating a post',err); return;}
+// module.exports.create = function(req, res){
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     }, function(err, post){
+//         if(err){console.log('error in creating a post',err); return;}
 
-        return res.redirect('back');
-    });
-}
+//         return res.redirect('back');
+//     });
+// }
 //are you there ? wait okay open robo3T ok 
 
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
-        // .id means converting the object id into string
-        if (post.user == req.user.id){
-            post.remove();
+// module.exports.destroy = function(req, res){
+//     Post.findById(req.params.id, function(err, post){
+//         // .id means converting the object id into string
+//         if (post.user == req.user.id){
+//             post.remove();
 
-            Comments.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            });
-        }else{
-            return res.redirect('back');
-        }
+//             Comments.deleteMany({post: req.params.id}, function(err){
+//                 return res.redirect('back');
+//             });
+//         }else{
+//             return res.redirect('back');
+//         }
 
-    });
-}
+//     });
+// }
 //dekho generally jb hm get rwquest bhejte hain toh uske sath kio additional information nhi bhejte hain,but agar hme addtional data behjna get request k sath jaisa ki hm post request me bhejde hain toh params aur query use krte hain......
 //okay aur bhi hai , if you want aap mic on kar sakte ho ? acyaat utyplle ykar loge ?
 //actully mere microphone kharab ho chuka hai...a
@@ -37,3 +37,88 @@ module.exports.destroy = function(req, res){
 //any time before 12..
 //okay ill give you a ping after 10 mins till then you can solve other's doubt  ?.o.kaoykay  no problem
 //tatatat bye :) thanks ...
+
+
+// module.exports.create =  async function(req, res){
+
+//                 try{
+//                     await Post.create({
+//                     content: req.body.content,
+//                     user: req.user._id
+//                     })
+
+//                  } 
+//                  catch(err){
+
+//                     console.log('ERROR',err);
+//                     return;
+//                 }
+
+
+//     }
+
+
+//     module.exports.destroy =  async function(req, res){
+        
+//         try{
+//         let post = await Post.findById(req.params.id);
+
+//                     if (post.user == req.user.id){
+//                         post.remove();
+
+//                        await  Comments.deleteMany({post: req.params.id});
+//                             return res.redirect('back');
+                        
+//                     }
+//                     else{
+//                         return res.redirect('back');
+//                         }
+
+//         }
+//         catch(err){
+//                         console.log('ERROR',err);
+//                         return;
+//         }
+        
+//     }
+
+
+    module.exports.create = async function(req, res){
+        try{
+            await Post.create({
+                content: req.body.content,
+                user: req.user._id
+            });
+        
+            return res.redirect('back');
+    
+        }catch(err){
+            console.log('Error', err);
+            return;
+        }
+      
+    }
+    
+    
+    module.exports.destroy = async function(req, res){
+    
+        try{
+            let post = await Post.findById(req.params.id);
+    
+            if (post.user == req.user.id){
+                post.remove();
+    
+                await Comment.deleteMany({post: req.params.id});
+                return res.redirect('back');
+            }else{
+                return res.redirect('back');
+            }
+    
+        }catch(err){
+            console.log('Error', err);
+            return;
+        }
+        
+    }
+
+
